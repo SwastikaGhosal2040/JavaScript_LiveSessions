54b2071b49b9fa1d58f3a1c69181d429d08996ac
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-22 23:07:17 +0530

    Update A_arrayMethods.js

--- 12thJan_ArrayMethods/A_arrayMethods.js
+++ 12thJan_ArrayMethods/A_arrayMethods.js
@@ -6,3 +6,8 @@ console.log(arr)  //prints the array
 console.log(arr.pop())  //prints the last element
 
 console.log(arr)  //prints array without the last element
+
+Output:
+[ 3, 4, 5, 6, 7, 8 ]
+8
+[ 3, 4, 5, 6, 7 ]
