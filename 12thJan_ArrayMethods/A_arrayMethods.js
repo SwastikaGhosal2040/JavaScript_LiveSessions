77dc6b76e59f96771ee6dedaef9058e80dab1bd8
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-22 23:03:18 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/A_arrayMethods.js
@@ -0,0 +1,8 @@
+//predict the output:
+arr = [3,4,5,6,7,8]
+
+console.log(arr)  //prints the array
+
+console.log(arr.pop())  //prints the last element
+
+console.log(arr)  //prints array without the last element
