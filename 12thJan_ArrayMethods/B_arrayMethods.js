2d49e3a03fb185569afc5593b6b80d07d112953a
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-23 07:55:48 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/B_arrayMethods.js
@@ -0,0 +1,24 @@
+//Predict the output:
+
+//Array push() Method
+
+arr = [3,4,5,6,7,8]
+
+console.log(arr.pop())  //prints the last element
+
+console.log(arr)  //prints array without the last element
+
+
+// add 11 at the end of the array
+
+console.log(arr.push(11)) //-> It will return the length of the new modified array.
+
+console.log(arr)
+
+/**
+ * Output:
+ * 8
+ * [ 3, 4, 5, 6, 7 ]
+ * 6
+ * [ 3, 4, 5, 6, 7, 11 ]
+ */
