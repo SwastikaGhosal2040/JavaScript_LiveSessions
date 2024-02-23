93d485664d45e6be2f8e7f9805901d0eddbf0f14
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-23 19:30:59 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/D_arrayMethods.js
@@ -0,0 +1,16 @@
+//Predict the output:
+fruits = ["Mango", "Apple", "Papaya"]
+
+f = fruits.pop()  // f = Papaya
+
+console.log(f)  //Papaya
+
+fruits.push("WaterMelon") //WaterMelon is added
+
+console.log(fruits) //['Mango', 'Apple', 'WaterMelon']
+
+/**
+ * Output:
+ * Papaya
+ * [ 'Mango', 'Apple', 'WaterMelon' ]
+ */
