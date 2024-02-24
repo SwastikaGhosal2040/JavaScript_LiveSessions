6412db1dc644585655a3cb4db9e3539c3bb5e755
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-24 06:27:18 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/E_arrayMethods.js
@@ -0,0 +1,20 @@
+//Predict the output:
+// Remove from the first position:
+
+
+arr = [5,2,1,9,7,6]
+
+x = arr.shift()  //Removes the 1st element of the array i.e. here 5, x = 5
+                 //Removes 0th index
+                 //elements are shifting left by 1 position
+
+console.log(x)   //5
+
+console.log(arr)  //[2,1,9,7,6]
+
+/**
+ * Output:
+ * 5
+ * [2,1,9,7,6]
+ */
+
