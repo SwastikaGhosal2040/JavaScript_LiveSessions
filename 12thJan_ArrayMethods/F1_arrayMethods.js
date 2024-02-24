07222a394bb602b9fba9937f63e7ff8fa51276ca
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-24 06:45:57 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/F1_arrayMethods.js
@@ -0,0 +1,19 @@
+//Predict the output:
+
+//Add elements1:
+  
+ arr = [5,2,1,9,7,6]
+ 
+ x = arr.shift() // x = 5
+  
+console.log(x) // 5
+ 
+arr.unshift(51) // in the shifted position 51 is included
+ 
+console.log(arr) // [51,2,1,9,7,6]
+
+/**
+ * Output:
+ * 5
+ * [51,2,1,9,7,6]
+ */
