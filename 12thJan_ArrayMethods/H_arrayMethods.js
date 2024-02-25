4a78bef14660771721105cbf537a160e4df73012
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-25 21:07:47 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/H_arrayMethods.js
@@ -0,0 +1,13 @@
+//Predict the output:
+
+
+ arr = [3,4,5,6,1,7]
+ arr.shift() 
+ 
+ //->array is shifted to left side by 1 element and 3 is out of the array
+
+ /**
+  * Output:
+  * nothing is printed.
+  */
+ 
