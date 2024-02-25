869f651f57176f381ee3ce3cc45398f2b8f472b9
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-25 21:10:15 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/H2_arrayMethods.js
@@ -0,0 +1,15 @@
+//Predict the output:
+
+
+arr = [3,4,5,6,1,7]
+console.log(arr.shift())    //3 
+console.log(arr)         //[4,5,6,1,7]
+
+
+//arr.shift() has higher complexity....
+
+/**
+ * Output:
+ * 3
+ * [ 4, 5, 6, 1, 7 ]
+ */
