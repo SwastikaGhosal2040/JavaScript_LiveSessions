433ab83d4beb0f9b3ceeffb54ad9292c1a3c7580
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-27 04:22:43 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/G3_arrayMethods.js
@@ -0,0 +1,16 @@
+//Predict the output:
+
+arr = [3,4,5,6,7,8]
+
+console.log(arr)
+
+console.log(arr.pop())
+
+console.log(arr)
+
+/**
+ * Output:
+ * [ 3, 4, 5, 6, 7, 8 ]
+ * 8
+ * [ 3, 4, 5, 6, 7 ]
+ */
