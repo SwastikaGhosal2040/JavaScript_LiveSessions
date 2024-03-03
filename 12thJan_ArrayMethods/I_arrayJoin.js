9ecdcdc97bdf5916c3ab4e958fc317272c609c0a
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-03-03 07:42:30 +0530

    Add files via upload

---
+++ 12thJan_ArrayMethods/I_arrayJoin.js
@@ -0,0 +1,19 @@
+//Predict the output:
+
+let fruits = ["Apple", "Banana", "Carrot"]
+
+let result = fruits.join()
+let result2 = fruits.join("-")
+
+console.log(result)
+console.log(typeof result)
+console.log(result2)
+console.log(typeof result2)
+
+/**
+ * Output:
+ * Apple,Banana,Carrot
+ * string
+ * Apple-Banana-Carrot
+ * string
+ */
